f879abf867daeee657f1e5af177d9c376f91baa1
nguyenduc <nguyenducdn67@gmail.com>
2015-03-07 09:59:33 +0700

    !update

--- app/views/site/user/create.blade.php
+++ app/views/site/user/create.blade.php
@@ -13,62 +13,62 @@
                 <h3 class="margin-b20 margin-t20">Create a Free Customer Account</h3>
                 @endif
                 <form method="POST" action="{{{ URL::to('user') }}}" accept-charset="UTF-8" class="form-horizontal ">
-    <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
-    <fieldset>
-        <div class="form-group ">
-            <label for="username" class="col-sm-3 control-label">{{{ Lang::get('confide::confide.username') }}}</label>
-            <div class="col-sm-9">
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.username') }}}" type="text" name="username" id="username" value="{{{ Input::old('username') }}}">
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="email" class="col-sm-3 control-label">{{{ Lang::get('confide::confide.e_mail') }}} </label>
-            <div class="col-sm-9">
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
-            </div>
-        </div>
+                    <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
+                        <fieldset>
+                            <div class="form-group ">
+                                <label for="username" class="col-sm-3 control-label">{{{ Lang::get('confide::confide.username') }}}</label>
+                                <div class="col-sm-9">
+                                    <input class="form-control" placeholder="{{{ Lang::get('confide::confide.username') }}}" type="text" name="username" id="username" value="{{{ Input::old('username') }}}">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="email" class="col-sm-3 control-label">{{{ Lang::get('confide::confide.e_mail') }}} </label>
+                                <div class="col-sm-9">
+                                    <input class="form-control" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
+                                </div>
+                            </div>
 
-        @if($type == 'trailer')
-            <div class="form-group">
-            <label for="Spa" class="col-sm-3 control-label">Spa name </label>
-            <div class="col-sm-9">
-                <input class="form-control" placeholder="Spa name" type="text" name="spaname" id="spaname" >
-            </div>
-        </div>
-        @endif
+                            @if($type == 'trailer')
+                                <div class="form-group">
+                                <label for="Spa" class="col-sm-3 control-label">Spa name </label>
+                                <div class="col-sm-9">
+                                    <input class="form-control" placeholder="Spa name" type="text" name="spaname" id="spaname" >
+                                </div>
+                            </div>
+                            @endif
 
-        <div class="form-group">
-            <label for="password" class="col-sm-3 control-label">{{{ Lang::get('confide::confide.password') }}}</label>
-            <div class="col-sm-9">
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password') }}}" type="password" name="password" id="password">
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="password_confirmation" class="col-sm-3 control-label">{{{ Lang::get('confide::confide.password_confirmation') }}}</label>
-            <div class="col-sm-9">
-                <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password_confirmation') }}}" type="password" name="password_confirmation" id="password_confirmation">
-            </div>
-        </div>
+                            <div class="form-group">
+                                <label for="password" class="col-sm-3 control-label">{{{ Lang::get('confide::confide.password') }}}</label>
+                                <div class="col-sm-9">
+                                    <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password') }}}" type="password" name="password" id="password">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="password_confirmation" class="col-sm-3 control-label">{{{ Lang::get('confide::confide.password_confirmation') }}}</label>
+                                <div class="col-sm-9">
+                                    <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password_confirmation') }}}" type="password" name="password_confirmation" id="password_confirmation">
+                                </div>
+                            </div>
 
-        @if (Session::get('error'))
-            <div class="alert alert-error alert-danger">
-                @if (is_array(Session::get('error')))
-                    {{ head(Session::get('error')) }}
-                @endif
-            </div>
-        @endif
+                            @if (Session::get('error'))
+                                <div class="alert alert-error alert-danger">
+                                    @if (is_array(Session::get('error')))
+                                        {{ head(Session::get('error')) }}
+                                    @endif
+                                </div>
+                            @endif
 
-        @if (Session::get('notice'))
-            <div class="alert">{{ Session::get('notice') }}</div>
-        @endif
-        <div class="form-group text-l">
-            <div  class="col-sm-3 control-label"></div>
-            <div class="col-sm-9">
-                <button type="submit" class="btn bt-spa">{{{ Lang::get('confide::confide.signup.submit') }}}</button>
-            </div>
-        </div>
-    </fieldset>
-</form>
+                            @if (Session::get('notice'))
+                                <div class="alert">{{ Session::get('notice') }}</div>
+                            @endif
+                            <div class="form-group text-l">
+                                <div  class="col-sm-3 control-label"></div>
+                                <div class="col-sm-9">
+                                    <button type="submit" class="btn bt-spa">{{{ Lang::get('confide::confide.signup.submit') }}}</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
 
                 </div>
             </div>
