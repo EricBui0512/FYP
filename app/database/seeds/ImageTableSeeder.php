1cae75712892004fe6df0ce10a3c9b23cfdd6212
cuongnp <kentmt@hotmail.com>
2015-03-10 10:56:22 +0700

    udpate data seed

--- app/database/seeds/ImageTableSeeder.php
+++ app/database/seeds/ImageTableSeeder.php
@@ -6,10 +6,11 @@ class ImageTableSeeder extends Seeder {
     {
         DB::statement("TRUNCATE TABLE images");
         
-        $image = new Image;
+        $image = new Picture;
         $image->image_path = 'asset/upload/D4C21840-5F68-48EA-A63E-7AC4C3D9B43A.JPG';
         $image->created_at = new DateTime;
         $image->updated_at = new DateTime;
+        $image->big_path   = 'asset/upload/D4C21840-5F68-48EA-A63E-7AC4C3D9B43A.JPG';
         $image->save();
     }
 
