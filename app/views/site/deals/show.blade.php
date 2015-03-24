d812dd40e6be29d68ec8d5ed86d0a23821969a15
troynguyen <tuanitus@gmail.com>
2015-03-24 11:26:42 +0700

    update map

--- app/views/site/deals/show.blade.php
+++ app/views/site/deals/show.blade.php
@@ -182,9 +182,10 @@
                               width="600"
                               height="450"
                               frameborder="0" style="border:0"
-                              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBvK67-gFTje3jAzNJAeCv60FU6xR4ZXGo
+                              src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBvK67-gFTje3jAzNJAeCv60FU6xR4ZXGo
                                 &q={{$detail->outletAddress}}, {{$detail->outletCity}}, {{$detail->outletCountry}}">
                             </iframe>
+
                       </div>
                     </div>
                 </div>
